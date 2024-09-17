58299e2a2a4208de2245a30ad4e75c729f10d6cd
Karthiktelagi <tskarthiktelagi@gmail.com>
2024-09-17 22:15:33 +0530

    Basic

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text(
+            "Home",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text("Karthik"), Text("Harsh")],
+          ),
+        ),
+      ),
+    );
+  }
+}
